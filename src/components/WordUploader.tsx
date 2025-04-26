421216cc507917a3f7ed672d57cafc121f3035ff
ArjunChaudhary4 <arjuntomar09161@gmail.com>
2025-04-26 18:11:57 +0530

    Add to button to show sample file for first time user

--- src/components/WordUploader.tsx
+++ src/components/WordUploader.tsx
@@ -2,12 +2,39 @@
 
 import { ChangeEvent } from "react";
 import { Word } from "../types";
+import { useState } from "react";
 
 interface WordUploaderProps {
   onUpload: (words: Word[]) => void;
 }
 
+
 export default function WordUploader({ onUpload }: WordUploaderProps) {
+  const [showImage, setShowImage] = useState(false);
+
+  const handleClick = () => {
+    setShowImage(prev => !prev);
+  };
+
+  const buttonStyle = {
+    padding: '10px 20px',
+    backgroundColor: showImage ? '#dc3545' : '#28a745',
+    color: '#fff',
+    border: 'none',
+    borderRadius: '5px',
+    cursor: 'pointer',
+    marginBottom: '10px',
+    transition: 'background-color 0.3s ease'
+  };
+  const imageStyle = {
+    width: '300px',
+    height: '200px',
+    objectFit: 'cover',
+    borderRadius: '8px',
+    boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)'
+  };
+
+
   const handleFileUpload = async (e: ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (!file) return;
@@ -30,6 +57,13 @@ export default function WordUploader({ onUpload }: WordUploaderProps) {
 
   return (
     <div className="mb-4">
+      <h2>Upload a txt file with word separated by a space, and new word in the next line</h2>
+      <button style={buttonStyle} onClick={handleClick}>
+        {showImage ? 'Hide sample file' : 'Show sample file'}
+      </button>
+      {showImage && (
+        <img src='../../assets/sample.png' alt="Sample" style={imageStyle}/>
+      )}
       <label className="block mb-2 font-medium">Upload Word List (.txt):</label>
       <input
         type="file"
